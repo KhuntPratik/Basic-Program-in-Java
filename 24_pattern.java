a1d6a03c93c73969b6d0ce11f9474d0f8ca4af79
KhuntPratik <pratikkhunt43@gmail.com>
2026-04-14 00:04:20 +0530

    solve pattern program bug

--- 24_pattern.java
+++ 24_pattern.java
@@ -4,13 +4,20 @@
 // 1 2 3 
 // 1 2 3 4
 
-class main{
-      public static void main(String[] args) {
-        for(int i=1 ; i<=4 ;i++){
-            for(int j=1 ; j<=i ;j++){
+import java.util.Scanner;
+
+class main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
             }
             System.err.println();
         }
-      }
+    }
 }
