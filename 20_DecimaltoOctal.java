1d250f1b64655611c737514062b66a1576b2fcca
KhuntPratik <pratikkhunt43@gmail.com>
2026-04-16 19:59:26 +0530

    Add program to convert Decimal to Octal

---
+++ 20_DecimaltoOctal.java
@@ -0,0 +1,17 @@
+// 20 WAP to Convert a Decimal to Octal and Vice versa.
+
+
+import java.util.Scanner;
+
+class DecimalToOctal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Decimal number: ");
+        int decimal = sc.nextInt();
+
+        String octal = Integer.toOctalString(decimal);
+
+        System.out.println("Octal number: " + octal);
+    }
+}
